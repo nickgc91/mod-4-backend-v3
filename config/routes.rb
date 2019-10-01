dd56e226480344fb23611300fb5c9a8555dc74b5
github username <github email address>
2019-10-01 13:16:50 +0100

    structured backend so that post request on frontent works now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, :scores
-
+  resources :users, only: [:index, :create]
+  post '/save-game', to: 'users#save_game'
 end
